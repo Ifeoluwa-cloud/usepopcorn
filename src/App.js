2620934810d7bf8c0e7ed9fc4222bad004d61f19
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-11-07 23:23:10 +0100

    Refractor NumResults Component

--- src/App.js
+++ src/App.js
@@ -51,20 +51,22 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 };
 
-function NavBar() {
+function NavBar({ movies }) {
   return(
     <nav className="nav-bar">
         <Logo />
         <Search />
-        <NumResults />
+        <NumResults movies={movies} />
     </nav>
   );
 }
@@ -92,24 +94,24 @@ function Search() {
   );
 }
 
-function NumResults(){
+function NumResults({ movies }){
   return(
     <p className="num-results">
-        Found <strong>X</strong> results
+        Found <strong>{movies.length}</strong> results
     </p>
   );
 }
 
-function Main() {
+function Main({ movies }) {
   return(
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox /> 
     </main>
   );
 }
 
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return(
@@ -120,14 +122,12 @@ function ListBox() {
             >
               {isOpen1 ? "–" : "+"}
             </button>
-            {isOpen1 && (<MovieList />)}
+            {isOpen1 && (<MovieList movies={movies} />)}
           </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
-
+function MovieList({ movies }) {
   return(
     <ul className="list">
                 {movies?.map((movie) => (
@@ -182,26 +182,26 @@ function WatchedSummary({ watched }) {
 
   return(
     <div className="summary">
-                  <h2>Movies you watched</h2>
-                  <div>
-                    <p>
-                      <span>#️⃣</span>
-                      <span>{watched.length} movies</span>
-                    </p>
-                    <p>
-                      <span>⭐️</span>
-                      <span>{avgImdbRating}</span>
-                    </p>
-                    <p>
-                      <span>🌟</span>
-                      <span>{avgUserRating}</span>
-                    </p>
-                    <p>
-                      <span>⏳</span>
-                      <span>{avgRuntime} min</span>
-                    </p>
-                  </div>
+        <h2>Movies you watched</h2>
+        <div>
+          <p>
+            <span>#️⃣</span>
+            <span>{watched.length} movies</span>
+          </p>
+          <p>
+            <span>⭐️</span>
+            <span>{avgImdbRating}</span>
+          </p>
+          <p>
+            <span>🌟</span>
+            <span>{avgUserRating}</span>
+          </p>
+          <p>
+            <span>⏳</span>
+            <span>{avgRuntime} min</span>
+          </p>
         </div>
+    </div>
   );
 }
 
