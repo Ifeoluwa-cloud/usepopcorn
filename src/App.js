702d1db89d09305eb15867206d1ff525ee359265
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-11-09 23:03:47 +0100

    StarRating Component Initialized

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default function App() {
         <Box>
           <WatchedSummary watched = {watched} />
           <WatchedMoviesList watched = {watched} />
-        </Box> 
+        </Box>  
       </Main>
     </>
   );
