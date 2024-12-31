fe9bb378c1389ca95e31a6177aadae9abee5f902
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-12-31 15:47:06 +0100

    Loader added to the App component

--- src/App.js
+++ src/App.js
@@ -55,13 +55,16 @@ const KEY = '187366d9'
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] =  useState(false);
   const query = "interstellar"
 
   useEffect(function() {
     async function fetchMovies() {
+        setIsLoading(true);
         const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
         const data = await res.json();
-        setMovies(data.Search);         
+        setMovies(data.Search);   
+        setIsLoading(false);      
     } 
     fetchMovies();
   }, []);
@@ -75,7 +78,7 @@ export default function App() {
       </NavBar>
       <Main>
           <Box>
-            <MovieList movies={movies} />
+            {isLoading ? <Loader /> : <MovieList movies={movies} />}
           </Box>
           
           <Box>
@@ -85,6 +88,10 @@ export default function App() {
       </Main>
     </>
   );
+};
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 };
 
 function NavBar({ children }) {
